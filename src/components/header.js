2a9fcf671eddf2023b2069dd3dcc4f1b6349a3b3
Jeppe Qin Hansen <jeppeqin@gmail.com>
2021-01-22 17:20:00 +0100

    cool header

--- src/components/header.js
+++ src/components/header.js
@@ -1,46 +1,65 @@
-import { Link } from "react-router-dom"
+import { useState, useEffect } from 'react'
+import { Link, useLocation } from "react-router-dom"
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import clsx from 'clsx'
 import { makeStyles } from '@material-ui/core/styles'
 import LocalBarIcon from '@material-ui/icons/LocalBar'
+import { motion, useAnimation } from 'framer-motion'
 
 const menuItems = [
   {
     name: 'OVERVIEW',
     path: '/overview',
-    active: false,
-    width: '180px'
+    left: '90px',
+    width: '180px',
+    activeWidth: '178px'
   },
   {
     name: 'TRADE',
     path: '/trade',
-    active: true,
-    width: '180px'
+    left: '270px',
+    width: '180px',
+    activeWidth: '178px'
   },
   {
     name: 'LEADERBOARD',
     path: '/leaderboard',
-    active: false,
-    width: '220px'
+    left: '450px',
+    width: '220px',
+    activeWidth: '218px'
   }
 ]
 
 function Header() {
   const classes = useStyles()
+  const location = useLocation()
+  const [active, setActive] = useState({})
+
+  useEffect(() => {
+    setActive(menuItems.find(item => location.pathname === item.path))
+  }, [location])
 
   return (
     <Grid container className={classes.header} justify='space-between' alignItems='center'>
       <Grid item>
         <Grid container>
+          <motion.div 
+            className={classes.activeContent}
+            animate={{ left: active.left, width: active.activeWidth }}
+            transition={{ duration: 0.2 }}
+          >
+            <Box className={classes.activeGlow} />
+          </motion.div>
           <Box className={clsx(classes.logo, classes.item)}>
             <LocalBarIcon className={classes.content} />
           </Box>
           {menuItems.map(item => {
             return <Link to={item.path} key={item.name} className={classes.item} style={{ width: item.width }}>
-              <Box className={classes.content}>{item.name}</Box>
-              {item.active && <Box className={classes.activeContent} />}
+              <Box className={classes.content} style={{ textShadow: item.name === active.name ? '0 0 5px white' : 'none' }}>
+                {item.name}
+              </Box>
             </Link>
           })}
         </Grid>
@@ -55,9 +74,8 @@ const useStyles = makeStyles({
   header: {
     width: '100%',
     height: '55px',
-    backgroundColor: '#1E2530',
+    background: 'linear-gradient(to left, rgba(30,37,48,0.2), rgba(30,37,48,1) 70%)',
     paddingRight: '10px',
-    boxShadow: '0px 2px 10px rgba(0, 255, 243, 0.1)',
     position: 'absolute',
     zIndex: '2'
   },
@@ -80,18 +98,26 @@ const useStyles = makeStyles({
     transform: 'skew(35deg)',
     textDecoration: 'none',
     color: 'white',
-    textShadow: '0 0 5px white'
-  },
-  glowText: {
   },
   activeContent: {
     position: 'absolute',
-    width: 'calc(100% - 2px)',
+    left: '200px',
+    width: '178px',
     height: '100%',
+    transform: 'skew(-35deg)',
     borderLeft: '4px solid rgba(255, 255, 255, 0.1)',
     borderRight: '4px solid rgba(255, 255, 255, 0.1)',
     borderTop: '3px solid rgba(255, 255, 255, 0.2)',
     borderBottom: '3px solid rgba(255, 255, 255, 0.2)',
+  },
+  activeGlow: {
+    position: 'absolute',
+    left: '25%',
+    top: '50%',
+    width: '50%',
+    height: '0px',
+    borderRadius: '50%',
+    boxShadow: '0px 0px 50px 20px rgba(0, 255, 243, 0.3)',
   },
   connect: {
     height: '38px',
