d3e84731ec256507591061c0953ce299fd930f8a
Jeppe Qin Hansen <jeppeqin@gmail.com>
2021-05-21 13:27:01 +0200

    add seasons page

---
+++ src/components/tables/scoreboardTable.js
@@ -0,0 +1,207 @@
+import React, { useState, useEffect, useContext } from 'react'
+import NumberFormat from 'react-number-format'
+import { withStyles, makeStyles } from '@material-ui/core/styles'
+import Skeleton from '@material-ui/lab/Skeleton'
+
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import ArrowBack from '@material-ui/icons/ArrowBackIos'
+import ArrowForward from '@material-ui/icons/ArrowForwardIos'
+
+import { styles } from './styles'
+import { SearchBar } from '../searchBar'
+import { HistoryDialog } from '../dialogs/historyDialog'
+import * as leaderboardApi from '../../api/leaderboard'
+import { TournamentContext } from '../../contexts/tournament'
+import { ellipseAddress } from '../../helpers/utilities'
+
+const PAGE_SIZE = 50
+
+const CustomTableCell = withStyles(() => ({
+  head: {
+    fontSize: 14
+  }
+}))(TableCell)
+
+const cells = [
+  { id: 'rank', label: '#', sortable: true },
+  { id: 'address', label: 'Player', sortable: true },
+  { id: 'networth', label: 'Networth', align: 'right', sortable: true },
+  { id: 'portfolio', label: 'Trades', align: 'center' },
+  { id: 'prize', label: 'Prize', align: 'center' },
+]
+
+export function ScoreBoard() {
+  const _classes = styles()
+  const classes = useStyles()
+
+  const [viewHistory, setViewHistory] = useState(null)
+  const [leaderboard, setleaderboard] = useState([])
+  const [total, setTotal] = useState(0)
+  const [page, setPage] = useState(0)
+  const [orderBy, setOrderBy] = useState()
+  const [direction, setDirection] = useState()
+  const [search, setSearch] = useState()
+  const [loading, setLoading] = useState(false)
+  const [first, setFirst] = useState(true)
+
+  const tournament = useContext(TournamentContext)
+
+  useEffect(() => {
+    async function fetchLeaderboard() {
+      setLoading(true)
+      const time = +new Date()
+
+      const data = await leaderboardApi.getLeaderboard(orderBy, direction, page, search)
+      setleaderboard(data.rows)
+      setTotal(data.count)
+      tournament.setPlayers(data.count)
+
+      if (first) {
+        setTimeout(() => {
+          setLoading(false)
+          setFirst(false)
+        }, Math.max(0,
+          500 - (+new Date() - time)))
+      } else {
+        setLoading(false)
+      }
+    }
+    fetchLeaderboard()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page, orderBy, direction, search])
+
+  function headerClick(id, sortable) {
+    if (!sortable || loading) {
+      return
+    }
+
+    if (orderBy === id) {
+      if (direction === 'DESC') {
+        setOrderBy(null)
+      } else {
+        setDirection('DESC')
+      }
+    } else {
+      setOrderBy(id)
+      setDirection('ASC')
+    }
+  }
+
+  function nextPage() {
+    if (page + 1 < total / PAGE_SIZE) {
+      setPage(page + 1)
+    }
+  }
+
+  function previousPage() {
+    if (page > 0) {
+      setPage(page - 1)
+    }
+  }
+
+  function loadingRow() {
+    return (
+      <TableRow>
+        {cells.map(cell => (
+          <TableCell key={'loading_' + cell.id}>
+            <Skeleton variant="text" animation="wave" />
+          </TableCell>
+        ))}
+      </TableRow>
+    )
+  }
+
+  function searchPlayer(address) {
+    if (address !== search) {
+      setPage(0)
+      setSearch(address)
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <Box mb={1} className={_classes.searchHeader}>
+        <Typography variant='h3'>Scoreboard</Typography>
+        <SearchBar search={value => searchPlayer(value)} value={search} placeholder={'Search address...'} />
+      </Box>
+      <Box className={_classes.tableContainer}>
+        <Table className={_classes.table}>
+          <TableHead>
+            <TableRow>
+              {cells.map(cell => (
+                <CustomTableCell key={cell.id} align={cell.align} onClick={() => headerClick(cell.id, cell.sortable)}>
+                  <Box style={{ cursor: cell.sortable ? 'pointer' : '', userSelect: 'none' }}>
+                    {orderBy === cell.id
+                      ? <Typography variant='body1' color='textSecondary' className={_classes.activeColumn}>{cell.label}</Typography>
+                      : <Typography variant='body1' color='textSecondary'>{cell.label}</Typography>
+                    }
+                  </Box>
+                </CustomTableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {first && loadingRow()}
+            {!first && leaderboard.map(row => (
+              <TableRow key={row.address}>
+                <TableCell>{row.rank}</TableCell>
+                <TableCell>
+                  <Button style={{ padding: '0px 5px' }}>
+                    {ellipseAddress(row.address, 8, 4)}
+                  </Button>
+                </TableCell>
+                <TableCell align='right'>
+                  <NumberFormat value={row.netWorth} displayType={'text'} thousandSeparator prefix={'$'} />
+                </TableCell>
+                <TableCell align='center'>
+                  <Box className={classes.viewLink} onClick={() => setViewHistory(row.address)}>VIEW</Box>
+                </TableCell>
+                <TableCell align='center'>
+                  <Typography>$1000</Typography>
+                </TableCell>
+              </TableRow>
+            ))}
+            <TableRow>
+              <TableCell />
+              <TableCell />
+              <TableCell />
+              <TableCell align='right'>
+                <Typography variant='body1' color='textSecondary'>{`${page * PAGE_SIZE + Math.min(leaderboard.length, 1)}-${page * PAGE_SIZE + leaderboard.length} of ${total}`}</Typography>
+              </TableCell>
+              <TableCell align='center'>
+                <Box display='flex' justifyContent='space-evenly'>
+                  <ArrowBack className={_classes.pageArrow} onClick={() => previousPage()} />
+                  <ArrowForward className={_classes.pageArrow} onClick={() => nextPage()} />
+                </Box>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Box>
+      {viewHistory && <HistoryDialog
+        open={viewHistory !== null}
+        close={() => setViewHistory(null)}
+        address={viewHistory}
+      />}
+    </React.Fragment>
+  )
+}
+
+const useStyles = makeStyles({
+  viewLink: {
+    cursor: 'pointer',
+    fontSize: '12px',
+    fontFamily: 'astrospace',
+    color: '#058665',
+    "&:hover": {
+      color: 'rgba(14, 70, 26, 0.9)'
+    }
+  },
+})
--- src/containers/seasons.js
+++ src/containers/seasons.js
@@ -5,39 +5,47 @@ import { Scrollbars } from 'react-custom-scrollbars'
 import NumberFormat from 'react-number-format'
 
 import Box from '@material-ui/core/Box'
-import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import Skeleton from '@material-ui/lab/Skeleton'
 
-import { LeaderboardTable } from '../components/tables/leaderboardTable'
+import { ScoreBoard } from '../components/tables/scoreboardTable'
 import { PlayerContext } from '../contexts/player'
 import { TournamentContext } from '../contexts/tournament'
 
-function Leaderboard() {
+function Seasons() {
   const classes = useStyles()
   const player = useContext(PlayerContext)
   const tournament = useContext(TournamentContext)
 
   return (
     <Scrollbars
-      renderThumbVertical={({ style, ...props }) => <div {...props} style={{ ...style, backgroundColor: '#fff', borderRadius: '5px', opacity: '0.4' }} />}
+      renderThumbVertical={({ style, ...props }) => <div {...props} style={{ ...style, backgroundColor: '#1E2530', borderRadius: '5px', opacity: '0.8' }} />}
+      autoHide
     >
-      <Grid container direction='column' alignItems='center' justify='center' className={classes.mainContainer}>
+      <Box className={classes.mainContainer}>
         <motion.div
           initial={{ opacity: 0.5 }}
           animate={{ opacity: 1 }}
           transition={{ ease: "linear", duration: 0.5 }}
         >
+          <Box className={classes.navigationContainer}>
+            <Box className={clsx(classes.customBox, classes.season)}>
+              <Typography variant='h3' style={{ fontSize: '15px' }}>season 1</Typography>
+            </Box>
+            <Box className={clsx(classes.customBox, classes.season, classes.inActive)}>
+              <Typography variant='h3' style={{ fontSize: '15px' }}>season 2</Typography>
+            </Box>
+          </Box>
           <Box className={classes.infoBoxContainer}>
             <Box className={clsx(classes.infoBox, classes.customBox)}>
-              <Typography variant='h6'>Net worth</Typography>
+              <Typography variant='h6'>Period</Typography>
               <Typography color='textPrimary' variant='h4'>
-                <NumberFormat value={player.info.cash + player.assetValue} decimalScale={2} displayType={'text'} thousandSeparator prefix={'$'} />
+                <Typography>May 1st - May 31st</Typography>
               </Typography>
             </Box>
             <Box className={clsx(classes.infoBox, classes.customBox)}>
-              <Typography variant='h6'>Position</Typography>
+              <Typography variant='h6'>Your Placement</Typography>
               <Typography color='textPrimary' variant='h4'>
                 <NumberFormat value={player.info.rank} displayType={'text'} prefix={'#'} thousandSeparator />
               </Typography>
@@ -52,21 +60,26 @@ function Leaderboard() {
               </Typography>
             </Box>
           </Box>
-          <LeaderboardTable />
+          <ScoreBoard />
         </motion.div>
-      </Grid >
-    </Scrollbars>
+      </Box>
+    </Scrollbars >
   )
 }
 
-export default Leaderboard
+export default Seasons
 
 const useStyles = makeStyles({
   mainContainer: {
+    display: 'flex',
+    justifyContent: 'center',
     width: '100%',
     marginTop: '100px',
     marginBottom: '50px',
     padding: '0 10px',
+    '@media (max-width: 1050px)': {
+      marginTop: '100px'
+    }
   },
   infoBox: {
     width: '330px',
@@ -78,10 +91,6 @@ const useStyles = makeStyles({
     padding: '20px',
     marginBottom: '15px'
   },
-  assetsContainer: {
-    width: '740px',
-    height: '270px'
-  },
   customBox: {
     backgroundColor: '#1E2530',
     borderLeft: '3px solid rgba(255, 255, 255, 0.1)',
@@ -89,9 +98,6 @@ const useStyles = makeStyles({
     borderTop: '2px solid rgba(255, 255, 255, 0.3)',
     borderBottom: '2px solid rgba(255, 255, 255, 0.3)',
     boxShadow: '0px 2px 10px rgba(0, 255, 243, 0.2)'
-  },
-  tableContainer: {
-    width: '1050px',
   },
   infoBoxContainer: {
     width: '1050px',
@@ -103,5 +109,26 @@ const useStyles = makeStyles({
       flexDirection: 'column',
       alignItems: 'flex-start'
     }
+  },
+  navigationContainer: {
+    height: '50px',
+    width: '100%',
+    marginBottom: '20px',
+    maxWidth: '1050px',
+    display: 'flex',
+  },
+  season: {
+    height: '100%',
+    width: '150px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: '20px',
+    position: 'relative',
+    cursor: 'pointer'
+  },
+  inActive: {
+    opacity: 0.4,
+    boxShadow: 'none'
   },
 });
